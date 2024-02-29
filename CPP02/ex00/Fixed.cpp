5eb583057ce07d681dceb86c753e3d18bb3731d1
gsilva <gustavocssilva1997@gmail.com>
2024-02-29 15:40:36 +0000

    c02e00

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -2,32 +2,36 @@
 
 Fixed::Fixed()
 {
-	this->nb = 0;
 	std::cout << "Default constructor called" << std::endl;
+	this->nb = 0;
 }
 
 Fixed::Fixed(Fixed& nb)
 {
-	this->nb = nb.nb;
 	std::cout << "Copy constructor called" << std::endl;
+	this->operator=(nb);
 }
 
 Fixed&	Fixed::operator=(Fixed& nb)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
+	this->nb = nb.getRawBits();
+	return nb;
 }
 
 Fixed::~Fixed()
 {
-
+	std::cout << "Destructor called" << std::endl;
 }
 
 int	Fixed::getRawBits(void) const
 {
 	std::cout << "getRawBits member function called" << std::endl;
+	return this->nb;
 }
 
 void	Fixed::setRawBits(int const raw)
 {
-	std::cout << "Copy constructor called" << std::endl;
+	std::cout << "setRawBits member function called" << std::endl;
+	this->nb = raw;
 }
