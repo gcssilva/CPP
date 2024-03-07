73c48b474eb9c154fbbedc3b22fc532c56718d36
gsilva <gustavocssilva1997@gmail.com>
2024-03-07 15:11:05 +0000

    c03e00

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -2,10 +2,79 @@
 
 ClapTrap::ClapTrap(std::string name) : hit_points_(10), energy_points_(10), attack_damage_(0)
 {
+	std::cout << "Default constructor called" << std::endl;
 	this->name_ = name;
 }
 
 ClapTrap::ClapTrap(const ClapTrap& ct)
 {
-	
+	std::cout << "Copy constructor called" << std::endl;
+	this->name_ = ct.getName();
+	this->hit_points_ = ct.getHP();
+	this->energy_points_ = ct.getEP();
+	this->attack_damage_ = ct.getAD();
+}
+
+void	ClapTrap::attack(const std::string& target)
+{
+	if (this->hit_points_ == 0){
+		std::cout << this->name_ << " is dead!" << std::endl;
+		return ;
+	}
+	if (this->energy_points_ > 0){
+		std::cout << this->name_ << " attacks " << target << ", causing " << this->attack_damage_ << " points of damage!" << std::endl;
+		this->energy_points_--;
+	}
+	else{
+		std::cout << this->name_ << " has no energy!" << std::endl;
+	}
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	if (this->hit_points_ == 0){
+		std::cout << this->name_ << " is dead!" << std::endl;
+		return ;
+	}
+	if (amount > this->hit_points_){
+		std::cout << this->name_ << " takes " << this->hit_points_ << " points of damage!" << std::endl;
+		this->hit_points_ = 0;
+	}
+	else{
+		std::cout << this->name_ << " takes " << amount << " points of damage!" << std::endl;
+		this->hit_points_ -= amount;
+	}
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->hit_points_ == 0){
+		std::cout << this->name_ << " is dead!" << std::endl;
+		return ;
+	}
+	if (amount > this->energy_points_){
+		std::cout << this->name_ << " restores " << this->energy_points_ << "hit points!" << std::endl;
+		this->hit_points_ += this->energy_points_;
+		this->energy_points_ = 0;
+	}
+}
+
+std::string	ClapTrap::getName(void) const
+{
+	return this->name_;
+}
+
+int	ClapTrap::getHP(void) const
+{
+	return this->hit_points_;
+}
+
+int	ClapTrap::getEP(void) const
+{
+	return this->energy_points_;
+}
+
+int	ClapTrap::getAD(void) const
+{
+	return this->attack_damage_;
 }
--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -21,7 +21,12 @@ public:
 	void	attack(const std::string& target);
 	void	takeDamage(unsigned int amount);
 	void	beRepaired(unsigned int amount);
+	std::string	getName(void) const;
+	int	getHP(void) const;
+	int	getEP(void) const;
+	int	getAD(void) const;
 	~ClapTrap();
+
 };
 
 #endif
