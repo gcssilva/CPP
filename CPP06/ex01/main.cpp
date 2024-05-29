d2d3d2c6c560bad71d8a36c8a260af480de0284a
gcssilva <gustavocssilva1997@gmail.com>
2024-05-29 15:42:18 +0100

    c06e01

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -4,13 +4,11 @@
 int	main()
 {
 	Data* data = new Data;
-	uintptr_t	serialized;
 	Data*	deserialized;
 	data->c = 'a';
 	data->i = 42;
 	data->s = "helo world";
-	serialized = Serializer::serialize(data);
-	deserialized = Serializer::deserialize(serialized);
+	deserialized = Serializer::deserialize(Serializer::serialize(data));
 	std::cout << "Data char = " << deserialized->c << "\nData int = " << deserialized->i << "\nData str = " << deserialized->s << std::endl;
 	delete data;
 	return 0;
